162fd3e423622704cfd2206be44fe05b62e7700b
klcodanr <daniel.klco@gmail.com>
2013-04-11 00:06:32 -0400

    Moving the inline js into a separate file and setting it to load the js async

---
+++ js/custom.js
@@ -0,0 +1,40 @@
+jQuery(function($){
+	$("#tweets").tweet({
+		username: "klcodanr",
+		count: 5
+	});
+	$('.client').each(function(index,elem){
+		if((index + 3) % 3 == 0){
+			$(elem).parent('div').addClass('offset-by-two');
+			$(elem).parent('div').css('clear','both');
+		}
+		$(elem).click(function(){
+			var id = $(elem).attr('id');
+			$.colorbox.close();
+			$('#client-box img').attr('src','/images/clients/'+id+'.png');
+			$('#client-box img').load(function(){
+				$.colorbox.resize();
+			});
+			var url = '/curriculum-vitae.html ';
+			if($(elem).attr('data-id')){
+				url += $(elem).attr('data-id');
+			} else {
+				url += '#'+id;
+			}
+			$('#client-box p').load(url, {}, function(){
+				$('#client-box').show();
+				$.colorbox({
+			        inline:true,
+			        href: '#client-box',
+			        title:$(elem).find('h5').text(),
+			        onClosed: function(){
+			        	$('#client-box').hide();
+			        	$('#client-box img').attr('src','/images/loading.gif');
+			        }
+			    });
+				$.colorbox.resize();
+			});
+			return false;
+		});
+	});
+});
