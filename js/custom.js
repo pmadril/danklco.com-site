acd31bd235b7ebfaad0fc788b51718e0d421bd27
Dan Klco <daniel.klco@gmail.com>
2013-09-10 02:15:25 -0400

    Update custom.js
    Adding a doc.ready since it seems like Chrome is trying to be clever with how the JS is loading

--- js/custom.js
+++ js/custom.js
@@ -1,36 +1,38 @@
 jQuery(function($){
-	$('.client').each(function(index,elem){
-		if((index + 3) % 3 == 0){
-			$(elem).parent('div').addClass('offset-by-two');
-			$(elem).parent('div').css('clear','both');
-		}
-		$(elem).click(function(){
-			var id = $(elem).attr('id');
-			$.colorbox.close();
-			$('#client-box img').attr('src','/images/clients/'+id+'.png');
-			$('#client-box img').load(function(){
-				$.colorbox.resize();
-			});
-			var url = '/curriculum-vitae.html ';
-			if($(elem).attr('data-id')){
-				url += $(elem).attr('data-id');
-			} else {
-				url += '#'+id;
+	$(document).ready(function(){
+		$('.client').each(function(index,elem){
+			if((index + 3) % 3 == 0){
+				$(elem).parent('div').addClass('offset-by-two');
+				$(elem).parent('div').css('clear','both');
 			}
-			$('#client-box p').load(url, {}, function(){
-				$('#client-box').show();
-				$.colorbox({
-			        inline:true,
-			        href: '#client-box',
-			        title:$(elem).find('h5').text(),
-			        onClosed: function(){
-			        	$('#client-box').hide();
-			        	$('#client-box img').attr('src','/images/loading.gif');
-			        }
-			    });
-				$.colorbox.resize();
+			$(elem).click(function(){
+				var id = $(elem).attr('id');
+				$.colorbox.close();
+				$('#client-box img').attr('src','/images/clients/'+id+'.png');
+				$('#client-box img').load(function(){
+					$.colorbox.resize();
+				});
+				var url = '/curriculum-vitae.html ';
+				if($(elem).attr('data-id')){
+					url += $(elem).attr('data-id');
+				} else {
+					url += '#'+id;
+				}
+				$('#client-box p').load(url, {}, function(){
+					$('#client-box').show();
+					$.colorbox({
+				        inline:true,
+				        href: '#client-box',
+				        title:$(elem).find('h5').text(),
+				        onClosed: function(){
+				        	$('#client-box').hide();
+				        	$('#client-box img').attr('src','/images/loading.gif');
+				        }
+				    });
+					$.colorbox.resize();
+				});
+				return false;
 			});
-			return false;
 		});
 	});
 });
